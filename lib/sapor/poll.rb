86135a03e26c94f67b4c1c7bfe8a6009d1d35189
f.a.vanlaenen@ieee.org <f.a.vanlaenen@ieee.org>
2015-09-02 16:59:19 +0200

    Added Greece to poll.rb

--- lib/sapor/poll.rb
+++ lib/sapor/poll.rb
@@ -29,9 +29,10 @@ module Sapor
 
     AREA_KEY = 'Area'
     AREAS_MAP = {}
-    [Catalonia.instance, Norway.instance, NorwegianMunicipality::BERGEN,
-     NorwegianMunicipality::OSLO, NorwegianMunicipality::TRONDHEIM,
-     UnitedKingdom.instance, Utopia.instance].map { |area| AREAS_MAP[area.area_code] = area }
+    [Catalonia.instance, Greece.instance, Norway.instance,
+     NorwegianMunicipality::BERGEN, NorwegianMunicipality::OSLO,
+     NorwegianMunicipality::TRONDHEIM, UnitedKingdom.instance,
+     Utopia.instance].map { |area| AREAS_MAP[area.area_code] = area }
 
     def initialize(metadata, results)
       @logger = LogFacade.create_logger
