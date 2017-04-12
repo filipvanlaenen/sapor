e7d55e0753b5da1de814cc65d66f64afd036f855
f.a.vanlaenen@ieee.org <f.a.vanlaenen@ieee.org>
2017-04-12 08:35:11 +0200

    Added France

---
+++ lib/sapor/regional_data/france.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+#
+# Statistical Analysis of Polling Results (SAPoR)
+# Copyright (C) 2016 Filip van Laenen <f.a.vanlaenen@ieee.org>
+#
+# This file is part of SAPoR.
+#
+# SAPoR is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# SAPoR is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You can find a copy of the GNU General Public License in /doc/gpl.txt
+#
+
+module Sapor
+  #
+  # The regional data for France.
+  #
+  class France < Area
+    include Singleton
+    def area_code
+      'FR'
+    end
+
+    def population_size
+      # Number of registered voters
+      # Source: French Wikipedia page about the French presidential elections in
+      # 2017, retrieved on 12 April 2017.
+      # https://fr.wikipedia.org/wiki/%C3%89lection_pr%C3%A9sidentielle_fran%C3%A7aise_de_2017
+      44_834_000
+    end
+  end
+end
---
+++ spec/unit/france_spec.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+#
+# Statistical Analysis of Polling Results (SAPoR)
+# Copyright (C) 2016 Filip van Laenen <f.a.vanlaenen@ieee.org>
+#
+# This file is part of SAPoR.
+#
+# SAPoR is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# SAPoR is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You can find a copy of the GNU General Public License in /doc/gpl.txt
+#
+
+require 'spec_helper'
+
+describe Sapor::France, '#area_code' do
+  it 'returns FR as the area code' do
+    expect(Sapor::France.instance.area_code).to eq('FR')
+  end
+end
+
+describe Sapor::France, '#population_size' do
+  it 'returns a population size of 44,834,000' do
+    expect(Sapor::France.instance.population_size).to eq(44_834_000)
+  end
+end
