bf54c3cd2f6ac90c72bb1ef9a7c0001413ce02e3
f.a.vanlaenen@ieee.org <f.a.vanlaenen@ieee.org>
2017-11-04 21:51:26 +0100

    Updated the specification for Catalonia with the results of the 2015 election

--- spec/unit/catalonia_spec.rb
+++ spec/unit/catalonia_spec.rb
@@ -20,7 +20,7 @@
 require 'spec_helper'
 
 describe Sapor::Catalonia, '#area_code' do
-  it 'returns ES-CAT as the area code' do
+  it 'returns ES-CT as the area code' do
     expect(Sapor::Catalonia.instance.area_code).to eq('ES-CT')
   end
 end
@@ -29,8 +29,11 @@ describe Sapor::Catalonia, '#coalitions' do
   it 'returns JS and JS+CUP as the coalitions to track' do
     expect(Sapor::Catalonia.instance.coalitions).to include(['Junts pel Sí'])
     expect(Sapor::Catalonia.instance.coalitions).to \
-      include(['Junts pel Sí',
-               "Candidatura d'Unitat Popular – Alternativa d'Esquerres"])
+      include(['Junts pel Sí', 'Candidatura d’Unitat Popular'])
+    expect(Sapor::Catalonia.instance.coalitions).to \
+      include(['Ciutadans–Partido de la Ciudadanía',
+               'Partit dels Socialistes de Catalunya (PSC-PSOE)',
+               'Catalunya en Comú', 'Partit Popular'])
   end
 end
 
@@ -41,37 +44,26 @@ describe Sapor::Catalonia, '#no_of_seats' do
 end
 
 describe Sapor::Catalonia, '#population_size' do
-  it 'returns a population size of 3,668,310' do
-    expect(Sapor::Catalonia.instance.population_size).to eq(3_668_310)
+  it 'returns a population size of 5,510,853' do
+    expect(Sapor::Catalonia.instance.population_size).to eq(5_510_853)
   end
 end
 
 describe Sapor::Catalonia, '#seats' do
-  it 'calculates the number of seats for the election of 2012 (adapted to' \
-     ' 2015) correctly' do
+  it 'calculates the number of seats for the election of 2015 correctly' do
     catalonia = Sapor::Catalonia.instance
-    results = catalonia.overall_election_results_of_2012_adapted_to_2015
+    results = catalonia.overall_election_results_of_2015
     seats = catalonia.seats(results)
-    expect(seats['Junts pel Sí']).to eq(64)
-    expect(seats['Partit dels Socialistes de Catalunya']).to eq(20)
-    expect(seats['Partit Popular de Catalunya']).to eq(19)
-    expect(seats['Catalunya Sí que es Pot']).to eq(13)
-    expect(seats['Ciutadans-Partido de la Ciudadanía']).to eq(9)
-    expect(seats['Unió Democràtica de Catalunya']).to eq(7)
-    expect(seats["Candidatura d'Unitat Popular – Alternativa d'Esquerres"]).to \
-      eq(3)
-    expect(seats['Plataforma per Catalunya']).to eq(0)
-    expect(seats['Solidaritat Catalana per la Independència']).to eq(0)
-    expect(seats['Escons en Blanc']).to eq(0)
-    expect(seats['Partido Animalista Contra el Maltrato Animal']).to eq(0)
-    expect(seats['Pirates de Catalunya']).to eq(0)
-    expect(seats['Unió, Progrés i Democràcia']).to eq(0)
-    expect(seats['Farts.cat']).to eq(0)
-    expect(seats['Via Democràtica']).to eq(0)
-    expect(seats["Unificació Comunista d'Espanya"]).to eq(0)
-    expect(seats["Partit Republicà d'Esquerra – Izquierda Republicana"]).to \
-      eq(0)
-    expect(seats['Socialistes i Republicans']).to eq(0)
+    expect(seats['Junts pel Sí']).to eq(62)
+    expect(seats['Ciutadans–Partido de la Ciudadanía']).to eq(25)
+    expect(seats['Partit dels Socialistes de Catalunya (PSC-PSOE)']).to eq(16)
+    expect(seats['Catalunya en Comú']).to eq(11)
+    expect(seats['Partit Popular']).to eq(11)
+    expect(seats['Candidatura d’Unitat Popular']).to eq(10)
+    expect(seats['Unió Democràtica de Catalunya']).to eq(0)
+    expect(seats['Partit Animalista contra el Maltractament Animal']).to eq(0)
+    expect(seats['Pirates de Catalunya–Per decidir-ho tot']).to eq(0)
+    expect(seats['Guanyem Catalunya']).to eq(0)
   end
 end
 
