117b51be812c0cd603ddb7efb5452c0e7b46b8cc
f.a.vanlaenen@ieee.org <f.a.vanlaenen@ieee.org>
2017-11-11 00:21:53 +0100

    Completed the electoral system for Hungary

---
+++ lib/sapor/regional_data/hungary.rb
@@ -0,0 +1,110 @@
+# encoding: utf-8
+#
+# Statistical Analysis of Polling Results (SAPoR)
+# Copyright (C) 2016 Filip van Laenen <f.a.vanlaenen@ieee.org>
+#
+# This file is part of SAPoR.
+#
+# SAPoR is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# SAPoR is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You can find a copy of the GNU General Public License in /doc/gpl.txt
+#
+
+module Sapor
+  #
+  # The regional data for Hungary.
+  #
+  class Hungary < Area
+    include Singleton
+
+    def area_code
+      'HU'
+    end
+
+    def individual_constituencies_seats(simulation)
+      individual_constituencies_electoral_system.project(simulation)
+    end
+
+    def no_of_seats
+      NO_OF_PROPORTIONAL_SEATS + \
+        individual_constituencies_election_results_of_2014.size
+    end
+
+    def overall_individual_constituencies_election_results_of_2014
+      if @overall_individual_constituencies_election_results_of_2014.nil?
+        @overall_individual_constituencies_election_results_of_2014 = \
+          summarize_election_results( \
+            individual_constituencies_election_results_of_2014
+          )
+      end
+      @overall_individual_constituencies_election_results_of_2014
+    end
+
+    def party_list_seats(simulation)
+      PARTY_LIST_ELECTORAL_SYSTEM.project(simulation)
+    end
+
+    def population_size
+      # Voter turnout on 6 April 2014
+      # Source: Wikipedia, Hungarian parliamentary election, 2014, downloaded
+      # on 10 November 2017,
+      # https://en.wikipedia.org/wiki/Hungarian_parliamentary_election,_2014
+      5_047_363
+    end
+
+    def seats(simulation)
+      individual_seats = individual_constituencies_seats(simulation)
+      party_seats = party_list_seats(simulation)
+      join_seats(individual_seats, party_seats)
+    end
+
+    def threshold
+      THRESHOLD
+    end
+
+    private
+
+    NO_OF_PROPORTIONAL_SEATS = 93
+    THRESHOLD = 0.05
+    PARTY_LIST_ELECTORAL_SYSTEM = SingleDistrictProportional.new(\
+      NO_OF_PROPORTIONAL_SEATS, DhondtDenominators, THRESHOLD
+    )
+
+    def individual_constituencies_election_results_of_2014
+      if @individual_constituencies_election_results_of_2014.nil?
+        @individual_constituencies_election_results_of_2014 = \
+          load_election_results('hungary-2014.psv')
+      end
+      @individual_constituencies_election_results_of_2014
+    end
+
+    def individual_constituencies_electoral_system
+      if @individual_constituencies_electoral_system.nil?
+        @individual_constituencies_electoral_system = FirstPastThePost.new(
+          overall_individual_constituencies_election_results_of_2014,
+          individual_constituencies_election_results_of_2014
+        )
+      end
+      @individual_constituencies_electoral_system
+    end
+
+    def join_seats(seats1, seats2)
+      results = seats1.dup
+      seats2.each_pair do |party, result|
+        if results.key?(party)
+          results[party] += result
+        else
+          results[party] = result
+        end
+      end
+      results
+    end
+  end
+end
---
+++ spec/unit/hungary_spec.rb
@@ -0,0 +1,118 @@
+# encoding: utf-8
+#
+# Statistical Analysis of Polling Results (SAPoR)
+# Copyright (C) 2016 Filip van Laenen <f.a.vanlaenen@ieee.org>
+#
+# This file is part of SAPoR.
+#
+# SAPoR is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# SAPoR is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You can find a copy of the GNU General Public License in /doc/gpl.txt
+#
+
+require 'spec_helper'
+
+describe Sapor::Hungary, '#area_code' do
+  it 'returns HU as the area code' do
+    expect(Sapor::Hungary.instance.area_code).to eq('HU')
+  end
+end
+
+describe Sapor::Hungary, '#individual_constituencies_seats' do
+  it 'calculates the number of seats in the individual constituencies for ' \
+     'the election of 2014 correctly' do
+    hungary = Sapor::Hungary.instance
+    results = hungary \
+              .overall_individual_constituencies_election_results_of_2014.dup
+    results['MSZP'] = results['MSZP'] * 5
+    results['DK'] = 1
+    results['Együtt'] = 1
+    results['MLP'] = 1
+    results['PM'] = 1
+    seats = hungary.individual_constituencies_seats(results)
+    expect(seats['Fidesz–KDNP']).to eq(87 + 9)
+    expect(seats['MSZP']).to eq(8 + 1 + 1 + 0 + 0)
+    expect(seats['Jobbik']).to eq(0)
+    expect(seats['LMP']).to eq(0)
+    expect(seats['Munkáspárt']).to eq(0)
+    expect(seats['ZÖP']).to eq(0)
+    expect(seats['Szociáldemokraták']).to eq(0)
+    expect(seats['ÖP']).to eq(0)
+    expect(seats['ÚDP']).to eq(0)
+    expect(seats['ÚMP']).to eq(0)
+  end
+end
+
+describe Sapor::Hungary, '#no_of_seats' do
+  it 'returns 199 as the number of seats' do
+    expect(Sapor::Hungary.instance.no_of_seats).to eq(199)
+  end
+end
+
+describe Sapor::Hungary, '#party_list_seats' do
+  it 'calculates the number of seats for the party lists for the election of ' \
+     '2014 correctly' do
+    hungary = Sapor::Hungary.instance
+    results = { 'Fidesz–KDNP' => 2_264_780, 'MSZP' => 1_290_806, 'DK' => 0,
+                'Együtt' => 0, 'MLP' => 0, 'PM' => 0, 'Jobbik' => 1_020_476,
+                'LMP' => 269_414, 'Munkáspárt' => 28_323, 'ZÖP' => 18_557,
+                'Szociáldemokraták' => 15_073, 'ÖP' => 6_552, 'ÚDP' => 2_100,
+                'ÚMP' => 1_578,
+                'Other' => 23_507 + 22_219 + 14_085 + 12_563 + 10_969 + \
+                           9_925 + 8_810 + 8_083 + 19_543 }
+    seats = hungary.party_list_seats(results)
+    expect(seats['Fidesz–KDNP']).to eq(44) # Was: 30 + 7
+    expect(seats['MSZP']).to eq(25) # Was: 21 + 2 + 3 + 1 + 1
+    expect(seats['Jobbik']).to eq(19) # Was: 23
+    expect(seats['LMP']).to eq(5)
+    expect(seats['Munkáspárt']).to eq(0)
+    expect(seats['ZÖP']).to eq(0)
+    expect(seats['Szociáldemokraták']).to eq(0)
+    expect(seats['ÖP']).to eq(0)
+    expect(seats['ÚDP']).to eq(0)
+    expect(seats['ÚMP']).to eq(0)
+  end
+end
+
+describe Sapor::Hungary, '#population_size' do
+  it 'returns a population size of 5,047,363' do
+    expect(Sapor::Hungary.instance.population_size).to eq(5_047_363)
+  end
+end
+
+describe Sapor::Hungary, '#seats' do
+  it 'calculates the number of seats for the election of 2014 correctly' do
+    hungary = Sapor::Hungary.instance
+    results = hungary \
+              .overall_individual_constituencies_election_results_of_2014.dup
+    results['MSZP'] = results['MSZP'] * 5
+    results['DK'] = 1
+    results['Együtt'] = 1
+    results['MLP'] = 1
+    results['PM'] = 1
+    seats = hungary.seats(results)
+    expect(seats['Fidesz–KDNP']).to eq(141)
+    expect(seats['MSZP']).to eq(37)
+    expect(seats['Jobbik']).to eq(21)
+    expect(seats['LMP']).to eq(0)
+    expect(seats['Munkáspárt']).to eq(0)
+    expect(seats['ZÖP']).to eq(0)
+    expect(seats['Szociáldemokraták']).to eq(0)
+    expect(seats['ÖP']).to eq(0)
+    expect(seats['ÚDP']).to eq(0)
+    expect(seats['ÚMP']).to eq(0)
+  end
+end
+
+describe Sapor::Hungary, '#threshold' do
+  it 'returns a threshold of 5%' do
+    expect(Sapor::Hungary.instance.threshold).to eq(0.05)
+  end
+end
