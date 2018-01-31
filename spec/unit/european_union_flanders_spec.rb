b31aef951ecb31c1f38d5a5adf098d75fceccd52
f.a.vanlaenen@ieee.org <f.a.vanlaenen@ieee.org>
2018-01-31 21:07:32 +0100

    Corrected the population size in the comment

--- spec/unit/european_union_flanders_spec.rb
+++ spec/unit/european_union_flanders_spec.rb
@@ -32,7 +32,7 @@ describe Sapor::EuropeanUnionFlanders, '#no_of_seats' do
 end
 
 describe Sapor::EuropeanUnionFlanders, '#population_size' do
-  it 'returns a population size of 4,171,869' do
+  it 'returns a population size of 4,212,371' do
     expect(Sapor::EuropeanUnionFlanders.instance.population_size).to \
       eq(4_212_371)
   end
