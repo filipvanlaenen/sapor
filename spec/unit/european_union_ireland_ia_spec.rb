83c9398884e34bb59cb9938cab78dbd3abfcb481
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-01-17 22:19:14 +0100

    Added Ireland variations as a European constituency for 2014 set-up

---
+++ spec/unit/european_union_ireland_ia_spec.rb
@@ -0,0 +1,40 @@
+# encoding: utf-8
+#
+# Statistical Analysis of Polling Results (SAPoR)
+# Copyright (C) 2016 Filip van Laenen <f.a.vanlaenen@ieee.org>
+#
+# This file is part of SAPoR.
+#
+# SAPoR is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# SAPoR is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You can find a copy of the GNU General Public License in /doc/gpl.txt
+#
+
+require 'spec_helper'
+
+describe Sapor::EuropeanUnionIrelandIa, '#area_code' do
+  it 'returns EU[IE]∪{IA} as the area code' do
+    expect(Sapor::EuropeanUnionIrelandIa.instance.area_code).to \
+      eq('EU[IE]∪{IA}')
+  end
+end
+
+describe Sapor::EuropeanUnionIrelandIa, '#no_of_seats' do
+  it 'returns 11 as the number of seats' do
+    expect(Sapor::EuropeanUnionIrelandIa.instance.no_of_seats).to eq(11)
+  end
+end
+
+describe Sapor::EuropeanUnionIrelandIa, '#population_size' do
+  it 'returns a population size of 1,656,518' do
+    expect(Sapor::EuropeanUnionIrelandIa.instance.population_size).to \
+      eq(1_656_518)
+  end
+end
