f124021358ad7b31d2259b6687a92c482327965d
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-10-24 21:12:56 +0200

    Issue #41: Moved Portuguese Aliança from * to RE

--- lib/sapor/regional_data/european_union_portugal.rb
+++ lib/sapor/regional_data/european_union_portugal.rb
@@ -46,13 +46,12 @@ module Sapor
 
     private
 
-    COALITIONS = [['Aliança (*)'],
+    COALITIONS = [['Aliança (RE)', 'Iniciativa Liberal (RE)'],
                   ['Bloco de Esquerda (GUE/NGL)',
                    'Coligação Democrática Unitária (GUE/NGL)'],
                   ['CDS–Partido Popular (EPP)',
                    'Partido Social Democrata (EPP)'],
                   ['Chega (ECR)'],
-                  ['Iniciativa Liberal (RE)'],
                   ['Partido Socialista (S&D)'],
                   ['LIVRE (Greens/EFA)',
                    'Pessoas–Animais–Natureza (Greens/EFA)']].freeze
