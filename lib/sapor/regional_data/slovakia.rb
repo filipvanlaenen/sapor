1fcdfd044fa01c3ced787695ec74022c41c52d46
Filip van Laenen <f.a.vanlaenen@ieee.org>
2020-02-14 20:15:27 +0100

    Added coalitions to Slovakia

--- lib/sapor/regional_data/slovakia.rb
+++ lib/sapor/regional_data/slovakia.rb
@@ -50,12 +50,41 @@ module Sapor
     private
     
     KDH_PARTY = 'Kresťanskodemokratické hnutie'.freeze
+    LSNS_PARTY = 'Kotleba–Ľudová strana Naše Slovensko'.freeze
     MH_PARTY = 'MOST–HÍD'.freeze
+    OLANO_PARTY = 'OBYČAJNÍ ĽUDIA a nezávislé osobnosti'.freeze
+    PS_SPOLU_PARTY = 'Progresívne Slovensko–SPOLU–Občianska Demokracia'.freeze
+    SAS_PARTY = 'Sloboda a Solidarita'.freeze
     SMER_SD_PARTY = 'SMER–sociálna demokracia'.freeze
+    SMK_MKP_PARTY = 'Strana maďarskej koalície–Magyar Koalíció Pártja'.freeze
     SNS_PARTY = 'Slovenská národná strana'.freeze
+    SR_PARTY = 'SME RODINA'.freeze
+    ZL_PARTY = 'Za ľudí'.freeze
     
-    COALITIONS = [[MH_PARTY, SMER_SD_PARTY, SNS_PARTY],
-                  [SMER_SD_PARTY]].freeze
+    COALITIONS = [[KDH_PARTY, OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY, SR_PARTY,
+                   ZL_PARTY],
+                  [KDH_PARTY, MH_PARTY, OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY,
+                   SMK_MKP_PARTY, SR_PARTY, ZL_PARTY],
+                  [KDH_PARTY, MH_PARTY, OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY,
+                   SMK_MKP_PARTY, ZL_PARTY],
+                  [KDH_PARTY, MH_PARTY, OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY,
+                   SR_PARTY, ZL_PARTY],
+                  [KDH_PARTY, MH_PARTY, OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY,
+                   ZL_PARTY],
+                  [KDH_PARTY, OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY,
+                   SMK_MKP_PARTY, SR_PARTY, ZL_PARTY],
+                  [KDH_PARTY, OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY,
+                   SMK_MKP_PARTY, ZL_PARTY],
+                  [KDH_PARTY, OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY, ZL_PARTY],
+                  [KDH_PARTY, PS_SPOLU_PARTY, SAS_PARTY, ZL_PARTY],
+                  [LSNS_PARTY, SMER_SD_PARTY, SNS_PARTY, SR_PARTY],
+                  [MH_PARTY, SMER_SD_PARTY, SNS_PARTY],
+                  [OLANO_PARTY, PS_SPOLU_PARTY, SAS_PARTY, ZL_PARTY],
+                  [PS_SPOLU_PARTY, SAS_PARTY, ZL_PARTY],
+                  [SMER_SD_PARTY],
+                  [SMER_SD_PARTY, SNS_PARTY],
+                  [SMER_SD_PARTY, SNS_PARTY, SR_PARTY],
+                  [SMER_SD_PARTY, SR_PARTY]].freeze
 
     NO_OF_SEATS = 150
 
